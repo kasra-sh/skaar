86ce6053b15d88ff4a59cefd6d7582f214cf528b
Kasra Shamsaei <kasra_sh94@protonmail.com>
2022-05-18 00:45:52 +0430

    refactor

--- packages/ui/jsx-runtime.ts
+++ packages/ui/jsx-runtime.ts
@@ -1,12 +1,9 @@
 import {createNode} from "./src/createNode"
 import {VElement, VElementProps} from "./src/vnode/VElement";
 import {VNode} from "./src/vnode/VNode";
-import {Component, ComponentClass} from "./src/component/Component";
+import {Component, ComponentClass, FnComponentClass} from "./src/component/Component";
 import {JsObject} from "./global";
-import {createComponent} from "./src/component/createComponent";
 import {Fragment} from "./src/vnode/Fragment";
-import {createVElement} from "./src/vnode/createVElement";
-import {translateJsxProperties} from "./src/translateJsxProperties";
 
 export function jsx(p1, p2, key) {
     if (p2 && p2.children && !Array.isArray(p2.children)) p2.children = [p2.children];
@@ -16,20 +13,13 @@ export function jsx(p1, p2, key) {
 
 export function jsxs(p1, p2, key) {
     if (p2) p2.key = key;
-    return createNode(p1, translateJsxProperties(p2), p2.children)
+    return createNode(p1, (p2), p2.children, true)
 }
 
 export function createElement(type: string, props: VElementProps, ...children: (VNode | string | unknown)[]): VElement;
 export function createElement(type: ComponentClass, props: JsObject, ...children: (VNode | string | unknown)[]): Component;
 export function createElement(type: null, props: null | undefined, ...children: (VNode | string | unknown)[]): Fragment;
-export function createElement(type: null | string | ComponentClass, props: VElementProps | JsObject, ...children: (VNode | string | unknown)[])
+export function createElement(type: null | undefined | string | ComponentClass | FnComponentClass, props: VElementProps | JsObject, ...children: (VNode | string | unknown)[])
     : Fragment | VElement | Component {
-    if (type === null) {
-        return new Fragment(children)
-    }
-    if (typeof type === "string") {
-        return createVElement(type, props as VElementProps, children)
-    }
-    return createComponent(type, props, children)
-
+    return createNode(type as any, props as any, children, true)
 }
