a0d41d4358c1ba4d9f14ee72d619ce8bd66c64ea
Kasra Shamsaei <kasra_sh94@protonmail.com>
2022-05-17 22:39:57 +0430

    implement basic store

---
+++ packages/ui/src/component/Store.ts
@@ -0,0 +1,54 @@
+type Selector = (state: Record<any, any>) => any
+
+type StoreSubscription = {
+    callback: (state: Record<any, any>) => void,
+    selector: Selector
+}
+
+type Reducer<T> = (state: T, payload: any) => void
+
+type StateChangedCallback<T> = (state: T) => void
+
+export class Store<T> {
+    state: T
+    subscriptions = Array<StoreSubscription>()
+    reducers: Record<any, Reducer<T>> = {
+        ['']: () => {}
+    }
+
+    constructor(initialState: T) {
+        this.state = initialState || {} as any
+    }
+
+    _triggerCallbacks(stateClone: T) {
+        for (const sub of this.subscriptions) {
+            sub.callback(stateClone)
+        }
+    }
+
+    subscribe(callback: StateChangedCallback<T>, selector?: Selector) {
+        this.subscriptions.push({callback, selector})
+        const clone = Object.assign({}, this.state)
+        setTimeout(()=>{
+            callback(clone)
+        })
+        return ()=>{
+            this.unsubscribe(callback)
+        }
+    }
+
+    unsubscribe(callback: StateChangedCallback<T>) {
+        this.subscriptions = this.subscriptions.filter(sub => sub.callback !== callback)
+    }
+
+    dispatch({action, payload=undefined}) {
+        const clone = Object.assign({}, this.state)
+        this.reducers[action](clone, payload)
+        Object.assign(this.state, clone)
+
+        setTimeout(()=>{
+            this._triggerCallbacks(clone)
+        })
+
+    }
+}
