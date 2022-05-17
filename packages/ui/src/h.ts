1378c7544f900df1a43deac5eef15da940c3bba9
Kasra Shamsaei <kasra_sh94@protonmail.com>
2022-05-18 00:45:57 +0430

    refactor

--- packages/ui/src/h.ts
+++ packages/ui/src/h.ts
@@ -5,7 +5,7 @@ import {Component, ComponentClass} from "./component/Component";
 import {JsObject} from "../global";
 import {createVElement} from "./vnode/createVElement";
 import {cloneComponent, createComponent} from "./component/createComponent";
-import {translateJsxProperties} from "./translateJsxProperties";
+import {transformJsxProperties} from "./transformJsxProperties";
 
 export function h(type: string, props?: JsObject, children?: (VNode | string | unknown)[]): VElement;
 export function h(type: ComponentClass | Component, props?: JsObject, children?: (VNode | string | unknown)[]): Component;
@@ -16,7 +16,7 @@ export function h(type: null | string | ComponentClass | Component, props?: JsOb
         return new Fragment(children)
     }
     if (typeof type === "string") {
-        return createVElement(type, translateJsxProperties(props as VElementProps), children)
+        return createVElement(type, transformJsxProperties(props as VElementProps), children)
     }
     if (typeof type === "function")
         return createComponent(type, props, children)
